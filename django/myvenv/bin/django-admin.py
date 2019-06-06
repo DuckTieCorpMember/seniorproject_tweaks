a706a83be7aa3e431303ad3799448284f1965168
DuckTieCorpMember <danyshbek1996@gmail.com>
2019-06-06 12:27:28 +0600

    Django Setup

---
+++ django/myvenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/assylbek/Desktop/face_recognition_overhaul/django/myvenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
