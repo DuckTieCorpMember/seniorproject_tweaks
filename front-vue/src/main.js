b3ce9ed17c3b5fe6063fb22a08577cd01644327c
DuckTieCorpMember <danyshbek1996@gmail.com>
2019-06-06 15:45:03 +0600

    End of Thursday

--- front-vue/src/main.js
+++ front-vue/src/main.js
@@ -1,9 +1,6 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
 import App from './App.vue'
-import 'vuetify/dist/vuetify.min.css' // Ensure you are using css-loader
 
-Vue.use(Vuetify)
 Vue.config.productionTip = false
 
 new Vue({
