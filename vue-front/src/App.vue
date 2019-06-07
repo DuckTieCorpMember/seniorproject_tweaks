22d7c2047a6d67cf4e60517b879cb3816b01c324
DuckTieCorpMember <danyshbek1996@gmail.com>
2019-06-07 18:43:03 +0600

    Friday FAIL

--- vue-front/src/App.vue
+++ vue-front/src/App.vue
@@ -1,8 +1,36 @@
 <template>
   <v-app>
-    <v-content>
-      
-    </v-content>
+    <v-container>
+      <v-layout row>
+        <v-flex shrink pa-1>
+          <v-card dark color="green darken-3">
+            <v-list two-line subheader>
+              <v-list-tile
+                v-for="item in items"
+                :key="item.title"
+                avatar
+                v-on:="">
+
+                <v-list-tile-avatar>
+                  <v-icon :class="[item.iconClass]">{{ item.icon }}</v-icon>
+                </v-list-tile-avatar>
+
+                <v-list-tile-content>
+                  <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                  <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+                </v-list-tile-content>
+
+              </v-list-tile>
+              </v-list>
+          </v-card>
+        </v-flex>
+        <v-flex grow pa-1>
+          <v-card dark color="green">
+            <v-card-text>Banana</v-card-text>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </v-app>
 </template>
 
@@ -14,8 +42,23 @@ export default {
   },
   data () {
     return {
-
+      items: [
+          { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: 'Photos'},
+          { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: 'Recipes'},
+          { icon: 'folder', iconClass: 'grey lighten-1 white--text', title: 'Work'}
+        ],
+        dirs: [],
     }
-  }
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
 }
 </script>
+
+<style scoped>
+
+</style>
