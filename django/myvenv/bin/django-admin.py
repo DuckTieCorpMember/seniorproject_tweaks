712628afd6e59e75e32f65ed428638b2831d98cb
DuckTieCorpMember <danyshbek1996@gmail.com>
2019-06-11 12:33:25 +0600

    Start API

--- django/myvenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/assylbek/Desktop/face_recognition_overhaul/django/myvenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
